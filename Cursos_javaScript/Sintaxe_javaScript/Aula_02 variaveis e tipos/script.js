5c4d68fab171fa3c6706b3597fb32570a3e72c5a
igor <diegoigor2001@gmail.com>
2022-07-19 21:39:17 -0300

    atualizando aulas de sintaxe

---
+++ Cursos_javaScript/Sintaxe_javaScript/Aula_02 variaveis e tipos/script.js
@@ -0,0 +1,92 @@
+//tipos primitivos
+
+// boolean: recebem valores que são verdadeiros ou falsos
+   /* var vOUF = false;
+    console.log(vOUF);  */
+
+    /*para saber o tipo da variavel usamos o metodo nativo typeof()
+    Ex: console.log(typeof(vOUF)); */
+
+
+//declarando variaveis
+    /*var variavel;
+    console.log(variavel);*/
+
+     /*let variavel2 = "maria";
+     variavel2 = "joão";
+     console.log(variavel2);*/
+
+     /*const CONSTANTE3 = 30;
+    console.log(CONSTANTE3);*/
+
+
+
+//Escopo
+    /*var escopoGlobal = "global";
+    console.log(escopoGlobal);
+
+    function escopoLocal(){
+        let escopolocalInterno = "local";
+        console.log(escopolocalInterno);
+    }
+    escopoLocal(); */
+
+
+//Atribuição é feita com os sinal de "="
+    /*var atribuicao = "valor";
+    console.log(atribuicao);*/
+
+
+//Comparação usamos o "==" e ele sempre vai retornar um valor bollean
+    /*var comparacao = 1 == 0;
+    console.log(comparacao);*/
+
+
+//Comparação indentica verificar tanto o valor quanto o tipo da variavel
+    /*var comparacaoIdentica = "0" === 0;
+    console.log(comparacaoIdentica);*/
+
+
+//Operadores Aritmeticos
+    /*var adicao = 1 + 2 ;
+    console.log(adicao);
+
+    var subtracao = 2 - 1;
+    console.log(subtracao);
+
+    var multiplicacao = 1 * 6;
+    console.log(multiplicacao);
+
+    var divisaoReal = 4 / 2;
+    console.log(divisaoReal);
+
+    var divisaoInterira = 9 % 2;
+    console.log(divisaoInterira);
+
+    var potenciacao = 4 ** 2;
+    console.log(potenciacao);*/
+
+
+//Operadores relacionais
+    /*var maiorQue = 5 > 2;
+    console.log(maiorQue);
+
+    var menorQue = 5 < 2;
+    console.log(menorQue);
+
+    var maiorOuIgual = 5 >= 2;
+    console.log(maiorOuIgual);
+
+    var menorOuIgual = 5 <= 5;
+    console.log(menorOuIgual);*/
+
+
+//Operadores logicos
+    /*var e = true && false;
+    console.log(e);
+
+    var ou = true || false;
+    console.log(ou);
+
+    var nao = !true;
+    console.log(nao);*/
